33d87b9ada45b332ebc5005beb8fff8cf8e853a0
candicedabre <dabrecandice@student.sfit.ac.in>
2024-10-25 10:52:22 +0530

    Update Start.js

--- src/components/Start.js
+++ src/components/Start.js
@@ -10,6 +10,7 @@ const Start = () => {
                 <div className="row vh-100 align-items-center justify-content-center">
                     <div className="col-lg-8">
                         <h1 className='fw-bold mb-4'>Basic React JS Quiz</h1>
+                        //start button
                         <button onClick={startQuiz} className="btn px-4 py-2 bg-light text-dark fw-bold">Start Quiz</button>
                     </div>
                 </div>
